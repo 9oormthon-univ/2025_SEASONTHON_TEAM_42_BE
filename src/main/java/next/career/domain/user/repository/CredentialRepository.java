b1dac50afdf378d69ffe20b06f0f5ec01cfc9c0b
seongho5356 <kjeng7897@gmail.com>
2025-08-29 19:51:47 +0900

    #2 Chore: 개발 전 필요없는 파일들을 삭제한다

--- src/main/java/next/career/domain/user/repository/CredentialRepository.java
+++
@@ -1,6 +0,0 @@
-package next.career.domain.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CredentialRepository extends JpaRepository<Credential, Long> {
-}

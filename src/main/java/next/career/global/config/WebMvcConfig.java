43a0a998d2e9a6b7997e384e6387a858aa4d7190
seongho5356 <kjeng7897@gmail.com>
2025-09-02 02:52:53 +0900

    #5 Chore: cors 설정을 작성한다

--- src/main/java/next/career/global/config/WebMvcConfig.java
+++ src/main/java/next/career/global/config/WebMvcConfig.java
@@ -19,6 +19,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "http://localhost:3000",
+                        "http://127.0.0.1:3000",
+                        "https://api.ilhaeng.cloud"
+                )
                 .allowedOrigins(devOrigin, prodOrigin)
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true)

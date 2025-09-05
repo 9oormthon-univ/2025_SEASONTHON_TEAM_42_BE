6118a5a19787cc0fa190febb1d7d323a23226a44
seongho5356 <kjeng7897@gmail.com>
2025-09-05 17:17:20 +0900

    #28 Feat: 북마크된 채용공고를 조회한다

--- src/main/java/next/career/domain/UserJobMap/service/BookMarkQueryService.java
+++ src/main/java/next/career/domain/UserJobMap/service/BookMarkQueryService.java
@@ -5,6 +5,7 @@ import next.career.domain.UserJobMap.repository.MemberJobMapRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.List;
 
 @Component

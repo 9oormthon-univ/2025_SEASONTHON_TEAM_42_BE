545b05d702f31c302c5707e81cc81becec5ae57d
seongho5356 <kjeng7897@gmail.com>
2025-09-05 16:33:55 +0900

    #28 Refactor: 서비스파일 이름 변경

---
+++ src/main/java/next/career/domain/UserJobMap/service/BookMarkQueryService.java
@@ -0,0 +1,21 @@
+package next.career.domain.UserJobMap.service;
+
+import lombok.RequiredArgsConstructor;
+import next.career.domain.UserJobMap.repository.MemberJobMapRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class BookMarkQueryService implements BookMarkFinder {
+
+    private final MemberJobMapRepository memberJobMapRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Long> findBookMarkedJobs(Long userId) {
+        return memberJobMapRepository.findJobIdsByUserId(userId);
+    }
+}

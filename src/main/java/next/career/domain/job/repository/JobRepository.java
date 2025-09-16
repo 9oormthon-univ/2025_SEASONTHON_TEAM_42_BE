8cfce38af6a5215f3f8d38079fd1e8d2add79f53
taek <wontaek88@gmail.com>
2025-09-16 11:10:57 +0900

    Feat: 일자리 데이터 저장 시 회사명,공고 제목 같은 일자리 필터링

--- src/main/java/next/career/domain/job/repository/JobRepository.java
+++ src/main/java/next/career/domain/job/repository/JobRepository.java
@@ -2,7 +2,12 @@ package next.career.domain.job.repository;
 
 import next.career.domain.job.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
 
+    @Query("SELECT CASE WHEN COUNT(j) > 0 THEN true ELSE false END " +
+            "FROM Job j " +
+            "WHERE j.jobTitle = :jobTitle AND j.companyName = :companyName")
+    boolean findAlreadyExists(String jobTitle, String companyName);
 }

2ea4effaeadb08c96f1305b4d9e8bfda81a2d158
taek <wontaek88@gmail.com>
2025-11-08 08:35:10 +0900

    #101 Feat: 비로그인 사용자 전용 교육 공고 조회 API를 추가한다

--- src/main/java/next/career/domain/education/controller/EducationController.java
+++ src/main/java/next/career/domain/education/controller/EducationController.java
@@ -68,7 +68,7 @@ public class EducationController {
         return ApiResponse.success();
     }
 
-    @GetMapping("/hrd-course")
+    @GetMapping("")
     @Operation(summary = "교육 조회", description = "교육을 조회합니다.")
     public ApiResponse<GetEducationDto.SearchAllResponse> raw(@RequestParam(defaultValue = "") String keyword,
                                                               @Parameter(hidden = true) Pageable pageable,
@@ -78,13 +78,12 @@ public class EducationController {
         return ApiResponse.success(educationService.getEducations(keyword, pageable, startYmd, endYmd, authDetails.getUser()));
     }
 
-//    @GetMapping("/all/anonymous")
-//    @Operation(summary = "전체 교육 조회 비로그인 사용자", description = "검색 조건과 페이징을 통해 전체 채용 공고 목록을 조회합니다.")
-//    public ApiResponse<GetEducationDto.SearchAllResponse> getAllJobAnonymous(@RequestParam(defaultValue = "") String keyword,
-//                                                                             @RequestParam(defaultValue = "1") int pageNo,
-//                                                                             @RequestParam(defaultValue = "10") int pageSize,
-//                                                                             @RequestParam(defaultValue = "20250101") String startYmd,
-//                                                                             @RequestParam(defaultValue = "20251231") String endYmd) {
-//        return ApiResponse.success(hrdCourseService.getEducations(keyword, pageNo, pageSize, startYmd, endYmd));
-//    }
+    @GetMapping("/anonymous")
+    @Operation(summary = "전체 교육 조회 비로그인 사용자", description = "검색 조건과 페이징을 통해 전체 채용 공고 목록을 조회합니다.")
+    public ApiResponse<GetEducationDto.SearchAllResponse> getAllJobAnonymous(@RequestParam(defaultValue = "") String keyword,
+                                                                             @Parameter(hidden = true) Pageable pageable,
+                                                                             @RequestParam(defaultValue = "20250101") String startYmd,
+                                                                             @RequestParam(defaultValue = "20251231") String endYmd) {
+        return ApiResponse.success(educationService.getEducationsForAnonymous(keyword, pageable, startYmd, endYmd));
+    }
 }
